69b12df2269f8aad3f4621bac33a56b4dcd8db38
tasheera <tasheeraabhishek@gmail.com>
2026-08-31 22:39:33 +0530

    Add form fields for email, subject, and description in SubmitPage component

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -3,25 +3,51 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { Textarea } from '@/components/ui/textarea'
+import { error } from 'console'
 import React, { useState } from 'react'
 
 type Props = {}
 
-function SubmitPage({}: Props) {
+function SubmitPage({ }: Props) {
   const [customerName, setCustomerName] = useState("");
+  const [customerEmail, setCustomerEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [description, setDescription] = useState("");
 
+
+  function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    //api
+  }
   return (
     <div className='flex min-h-screen justify-center px-4 py-12'>
       <Card className='w-full max-w-md'>
         <CardHeader >
-          <CardTitle>Submit a Ticket</CardTitle>
+          <CardTitle className='bold font-bold'>Submit a Ticket</CardTitle>
         </CardHeader>
         <CardContent >
-          <form  className='space-y-24'>
+          <form className='space-y-4'>
             <div className='flex flex-col gap-2'>
               <Label htmlFor="customerName">Name</Label>
-              <Input id='customerName' value={customerName} onChange={(e) => setCustomerName(e.target.value)}></Input>
+              <Input id='customerName' value={customerName} onChange={(e) => setCustomerName(e.target.value)} />
             </div>
+            <div className='flex flex-col gap-2'>
+              <Label htmlFor='customerEmail'>Email</Label>
+              <Input id='customerEmail' value={customerEmail} onChange={(e) => setCustomerEmail(e.target.value)} type='email' />
+
+            </div>
+            <div className='flex flex-col gap-2'>
+              <Label htmlFor='subject' >Subject</Label>
+              <Input id='subject' value={subject} onChange={(e) => setSubject(e.target.value)} />
+            </div>
+
+            <div className='flex flex-col gap-2'>
+              <Label htmlFor='description' >Description</Label>
+              <Textarea id='description' value={description} onChange={(e) => setDescription(e.target.value)} rows={5} />
+            </div>
+
           </form>
         </CardContent>
       </Card>
