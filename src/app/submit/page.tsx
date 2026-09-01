6f11ee0e2f6371cb71e0602de8a7e152d6c046a8
tasheera <tasheeraabhishek@gmail.com>
2026-09-01 12:09:06 +0530

    Add rest of validation and error handling to SubmitPage form fields

--- src/app/submit/page.tsx
+++ src/app/submit/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -14,11 +15,26 @@ function SubmitPage({ }: Props) {
   const [customerEmail, setCustomerEmail] = useState("");
   const [subject, setSubject] = useState("");
   const [description, setDescription] = useState("");
+  const [error, setError] = useState<Record<string, string>>({});
 
+  function validate() {
+    const newErrors: Record<string, string> = {};
+    if (!customerName.trim()) newErrors.customerName = "Name is required";
+    const emailInput = document.getElementById('customerEmail') as HTMLInputElement;
+    if (!emailInput.checkValidity()) {
+      newErrors.customerEmail = "Enter a valid email";
+    }
+    if (!subject.trim()) newErrors.subject = "Subject is required";
+    if (!description.trim()) newErrors.description = "Description is required";
+
+    setError(newErrors);
+    return Object.keys(newErrors).length === 0;
+
+  }
 
   function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
     e.preventDefault();
-
+    if (!validate()) return;
     //api
   }
   return (
@@ -28,26 +44,29 @@ function SubmitPage({ }: Props) {
           <CardTitle className='bold font-bold'>Submit a Ticket</CardTitle>
         </CardHeader>
         <CardContent >
-          <form className='space-y-4'>
+          <form onSubmit={handleSubmit} className='space-y-4'>
             <div className='flex flex-col gap-2'>
               <Label htmlFor="customerName">Name</Label>
               <Input id='customerName' value={customerName} onChange={(e) => setCustomerName(e.target.value)} />
+              {error.customerName && <p className='text-sm text-red-600'> {error.customerName}</p>}
             </div>
             <div className='flex flex-col gap-2'>
               <Label htmlFor='customerEmail'>Email</Label>
               <Input id='customerEmail' value={customerEmail} onChange={(e) => setCustomerEmail(e.target.value)} type='email' />
-
+              {error.customerEmail && <p className='text-sm text-red-600'> {error.customerEmail}</p>}
             </div>
             <div className='flex flex-col gap-2'>
               <Label htmlFor='subject' >Subject</Label>
               <Input id='subject' value={subject} onChange={(e) => setSubject(e.target.value)} />
+              {error.subject && <p className='text-sm text-red-600'> {error.subject}</p>}
             </div>
 
             <div className='flex flex-col gap-2'>
               <Label htmlFor='description' >Description</Label>
               <Textarea id='description' value={description} onChange={(e) => setDescription(e.target.value)} rows={5} />
+              {error.description && <p className="text-sm text-red-600 mt-1">{error.description}</p>}
             </div>
-
+            <Button type='submit' className="w-full"> Submit</Button>
           </form>
         </CardContent>
       </Card>
