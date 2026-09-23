999b4422e2623a0d0d301ba81c1e934e6b2e4e37
tasheera <tasheeraabhishek@gmail.com>
2026-09-23 14:43:04 +0530

    Creatre PagedResponse type definition

--- src/types/ticket.ts
+++ src/types/ticket.ts
@@ -11,3 +11,13 @@ export type Ticket = {
   status: string;
   createdAt: string;
 };
+
+export type PagedResponse<T> = {
+  items: T[];
+  page: number;
+  pageSize: number;
+  totalCount: number;
+  totalPages: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+};
